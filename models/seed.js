18c7f3f9892e885509c677ee908d777422a98022
annexus254 <info@gimaru.com>
2021-11-03 12:57:55 +0300

    Creating seed data

--- models/seed.js
+++ models/seed.js
@@ -1,1 +1,8 @@
-module.exports = {};
+module.exports = { 
+                    name : "serena", 
+                    location : "nairobi", 
+                    rating : 4, 
+                    vacancies : true,
+                    tags : ["Five-star" , "Luxury"],
+                    rooms : [{roomNumber: 105, size: "50 by 50", price: 80000, booked: false}]
+                 };
