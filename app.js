6ec3347af9b28fed0fcc2387b987e54a9a26dcf5
annexus254 <info@gimaru.com>
2021-11-03 12:58:28 +0300

    Adding the seed data to the database

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const db          =   mongoose.connection;
 
 // Config
 
-const mongoURI = "mongodb+srv://annexus:pass123@cluster0.ezy7s.mongodb.net/hotel?retryWrites=true&w=majority";
+const mongoURI    =   "mongodb+srv://annexus:pass123@cluster0.ezy7s.mongodb.net/hotel?retryWrites=true&w=majority";
 
 // Models
 
-const Hotel = require("./models/hotel.js");
-const hotelSeed = require("./models/seed.js");
+const Hotel       =   require("./models/hotel.js");
+const hotelSeed   =   require("./models/seed.js");
 
 
 // Connect to Mongo
@@ -27,6 +27,21 @@ mongoose.connect(
 
 db.on("error", (err) => console.log(err.message + " is Mongod not running?"));
 
-db.on("connected", () => console.log("mongo connected: ", mongoURI));
+db.on("connected", () => console.log("Mongo running at " + mongoURI + "\n\nConnection made!"));
 
 db.on("disconnected", () => console.log("mongo disconnected"));
+
+/**
+ * Adding seed data to the database
+ */
+/*
+  //Method 1
+Hotel.create( hotelSeed , function (err , serena){
+  if ( err ) console.log ( err.message );
+
+  console.log( "added provided hotel data" );
+})
+  //Method 2
+const serena  = new Hotel(hotelSeed);
+serena.save().then( ()=> console.log("added provided hotel data") ).catch(err=>console.log(err));
+*/
